aefa46ad88d9903271131c332d0fd34913cf6cb1
danzuep <82693586+danzuep@users.noreply.github.com>
2022-11-17 02:03:25 +0800

    Replaaced AddMailKitSimplifiedEmailSender with just IEmailWriter and ISmtpSender for now

--- samples/WorkerServiceExample/Program.cs
+++ samples/WorkerServiceExample/Program.cs
@@ -1,12 +1,8 @@
 using MailKitSimplified.Sender;
 using MailKitSimplified.Receiver;
-using MailKit;
-using MailKit.Net.Smtp;
 using MailKitSimplified.Sender.Abstractions;
 using MailKitSimplified.Sender.Models;
 using MailKitSimplified.Sender.Services;
-using Microsoft.Extensions.Configuration;
-using System.IO.Abstractions;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
@@ -15,7 +11,6 @@ IHost host = Host.CreateDefaultBuilder(args)
         var requiredSection = context.Configuration.GetRequiredSection(EmailSenderOptions.SectionName);
         services.Configure<EmailSenderOptions>(requiredSection);
         services.AddTransient<IEmailWriter, EmailWriter>();
-        services.AddTransient<ISmtpClient, SmtpClient>();
         services.AddTransient<ISmtpSender, SmtpSender>();
         //services.AddMailKitSimplifiedEmailSender(context.Configuration);
         services.AddMailKitSimplifiedEmailReceiver(context.Configuration);
