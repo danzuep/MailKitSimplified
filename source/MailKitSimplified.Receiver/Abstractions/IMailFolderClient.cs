97e942240e4e84216735194c8547cd9324d777ac
danzuep <82693586+danzuep@users.noreply.github.com>
2022-12-12 08:40:39 +0800

    Removed IMailFolderClient.SearchAsync(), use IMailReader.Query() instead.

--- source/MailKitSimplified.Receiver/Abstractions/IMailFolderClient.cs
+++ source/MailKitSimplified.Receiver/Abstractions/IMailFolderClient.cs
@@ -1,7 +1,5 @@
 ﻿using MailKit;
-using MailKit.Search;
 using System;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -16,14 +14,6 @@ namespace MailKitSimplified.Receiver.Abstractions
         /// <param name="cancellationToken">Mail folder connection cancellation token.</param>
         /// <returns>Connected <see cref="IMailFolder"/>.</returns>
         ValueTask<IMailFolder> ConnectAsync(bool enableWrite = false, CancellationToken cancellationToken = default);
-
-        /// <summary>
-        /// Query the server for the unique IDs of messages with properties that match the search filters.
-        /// </summary>
-        /// <param name="searchQuery">Mail folder search query.</param>
-        /// <param name="cancellationToken">Cancellation token.</param>
-        /// <returns>The first 250 <see cref="UniqueId"/>s.</returns>
-        Task<IList<UniqueId>> SearchAsync(SearchQuery searchQuery, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Mail folder summary.
