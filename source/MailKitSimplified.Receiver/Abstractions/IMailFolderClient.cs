3aa0808fbbc33bf6565933943737b753a15e31ba
danzuep <82693586+danzuep@users.noreply.github.com>
2022-12-12 10:16:03 +0800

    Revert "Removed IMailFolderClient.SearchAsync(), use IMailReader.Query() instead."
    This reverts commit 97e942240e4e84216735194c8547cd9324d777ac.

--- source/MailKitSimplified.Receiver/Abstractions/IMailFolderClient.cs
+++ source/MailKitSimplified.Receiver/Abstractions/IMailFolderClient.cs
@@ -1,5 +1,7 @@
 ﻿using MailKit;
+using MailKit.Search;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,6 +16,14 @@ namespace MailKitSimplified.Receiver.Abstractions
         /// <param name="cancellationToken">Mail folder connection cancellation token.</param>
         /// <returns>Connected <see cref="IMailFolder"/>.</returns>
         ValueTask<IMailFolder> ConnectAsync(bool enableWrite = false, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Query the server for the unique IDs of messages with properties that match the search filters.
+        /// </summary>
+        /// <param name="searchQuery">Mail folder search query.</param>
+        /// <param name="cancellationToken">Cancellation token.</param>
+        /// <returns>The first 250 <see cref="UniqueId"/>s.</returns>
+        Task<IList<UniqueId>> SearchAsync(SearchQuery searchQuery, CancellationToken cancellationToken = default);
 
         /// <summary>
         /// Mail folder summary.
