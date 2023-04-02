2a9fedebd7dbfc4ffca144f7fcce117d92092027
danzuep <82693586+danzuep@users.noreply.github.com>
2023-04-02 22:42:15 +0800

    Environment.NewLine.Length

--- tests/MailKitSimplified.Receiver.Tests/MailKitProtocolLoggerUnitTests.cs
+++ tests/MailKitSimplified.Receiver.Tests/MailKitProtocolLoggerUnitTests.cs
@@ -12,7 +12,7 @@ namespace MailKitSimplified.Receiver.Tests
         internal static readonly string LogFilePath = @"Logs\ImapClient.txt";
         internal static readonly string TestReply = $"* OK smtp4dev{Environment.NewLine}A00000000 CAPABILITY{Environment.NewLine}";
         internal readonly int TestReplyMid = 17 - Environment.NewLine.Length;
-        internal readonly int TestReplyEnd = 23 - Environment.NewLine.Length;
+        internal readonly int TestReplyEnd = 25 - (Environment.NewLine.Length * 2);
         private static readonly Uri _localhost = new Uri("imap://localhost:143/?starttls=when-available");
         private static readonly byte[] _testBytes = Encoding.UTF8.GetBytes(TestReply);
         private readonly ILoggerFactory _loggerFactory;
